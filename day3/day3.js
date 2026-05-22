b2a4ddefe6c62e2c7be7d7361d726ccd606ba74d
ShchcerbakoVV <ilya.shcherbakov.05@list.ru>
2026-05-22 21:27:13 +0300

    end_daY3

---
+++ day3/day3.js
@@ -0,0 +1,41 @@
+const upbtn = document.querySelector(".up-button");
+const downbtn = document.querySelector(".down-button");
+const  sizeWind = document.querySelector(".container");
+const sidebar = document.querySelector(".sidebar");
+const mainSlide = document.querySelector(".main-slide");
+
+
+const SlideCount = mainSlide.querySelectorAll('div').length
+
+
+
+sidebar.style.top = `-${(SlideCount-1)*100}vh`;
+let res = 0 ;
+
+upbtn.addEventListener("click", (e) => {
+    chanchSlide('up' );
+})
+
+downbtn.addEventListener("click", (e) => {
+    chanchSlide('down');
+})
+
+function chanchSlide( direction) {
+    if( direction === "up" ) {
+        res++
+        if(res === SlideCount){
+            res = 0 ;
+        }
+    }else if( direction === "down"){
+        res--
+        if(res < 0){
+            res = SlideCount -1;
+        }
+    }
+
+    const height = sizeWind.offsetHeight;
+    // Анимация
+    mainSlide.style.transform = `translateY(-${height*res}px)`;
+   sidebar.style.transform = `translateY(${height*res}px)`;
+
+}
